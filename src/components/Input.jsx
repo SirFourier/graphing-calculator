19b3497944857908e967a3bb19f13fc7249b562a
Ryan Tabar <70136507+SirFourier@users.noreply.github.com>
2022-04-13 04:22:53 +0100

    generalised component further

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,11 +1,29 @@
 import React from "react";
 
-export default function Input({ id, type, value, onChange, onDelete }) {
+export default function Input({
+  id,
+  label,
+  type,
+  placeholder,
+  value,
+  onChange,
+  onDelete,
+}) {
   return (
     <div className="input-group flex-nowrap mb-2">
-      <span className="input-group-text" >{id}</span>
-      <input className="form-control" type={type} value={value} onChange={(e) => onChange(id, e)} />
-      <button className="btn btn-outline-danger" onClick={() => onDelete(id)}>Delete</button>
+      {label && <span className="input-group-text">{label}</span>}
+      <input
+        className="form-control"
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={(e) => onChange(id, e)}
+      />
+      {onDelete && (
+        <button className="btn btn-outline-danger" onClick={() => onDelete(id)}>
+          Delete
+        </button>
+      )}
     </div>
   );
 }
