dd1adcfaa7441c251e4ef8c8c17f6c54a6113e60
Ryan Tabar <70136507+SirFourier@users.noreply.github.com>
2022-04-13 04:23:08 +0100

    added search and response sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,57 @@
 import React, { useState } from "react";
-import Inputs from "./components/Inputs";
+import Input from "./components/Input";
+
+// Using openweathermap.org
+const API_KEY = "5d5c88002278a4fbef29d7752e855e0e";
+
+const getAPI_RequestString = (city) =>
+  `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+
+const emptyQueryData = {
+  country: "",
+  city: "",
+};
 
 export default function App() {
-  const [inputs, setInputs] = useState([""]);
+  const [queryData, setQueryData] = useState(emptyQueryData);
+  const [responseData, setResponseData] = useState({});
 
   const handleChange = (id, e) => {
-    const newInputs = [...inputs];
-    newInputs[id] = e.target.value;
-    setInputs(newInputs);
+    const newData = { ...queryData };
+    newData[id] = e.target.value;
+    setQueryData(newData);
   };
 
-  const handleDelete = (id) => {
-    const newInputs = [...inputs];
-    newInputs.splice(id, 1);
-    setInputs(newInputs);
-  };
+  const handleSearch = async () => {
+    const api = await fetch(getAPI_RequestString(queryData.city));
+    const data = await api.json();
 
-  const handleAdd = () => {
-    setInputs([...inputs, ""]);
+    const newResponseData = { ...responseData, ...data.main };
+    setResponseData(newResponseData);
   };
 
   return (
-    <div className="container">
-      <Inputs
-        type="text"
-        inputs={inputs}
-        onChange={handleChange}
-        onDelete={handleDelete}
-        onAdd={handleAdd}
-      />
+    <div className="container mt-5">
+      <h1 align="center">Weather App</h1>
+      {Object.keys(queryData).map((key) => (
+        <Input
+          key={key}
+          id={key}
+          label={key}
+          type="text"
+          placeholder="Input text here"
+          value={queryData[key]}
+          onChange={handleChange}
+        />
+      ))}
+      <button className="btn btn-primary align-self-end" onClick={handleSearch}>
+        Search
+      </button>
+      {Object.keys(responseData).map((key) => (
+        <p key={key}>
+          {key}: {responseData[key]}
+        </p>
+      ))}
     </div>
   );
 }
