f9be0d76a1f52f66de223c593eef4b6c5005efe7
Ryan Tabar <70136507+SirFourier@users.noreply.github.com>
2022-04-14 10:34:11 +0100

    error handling complete

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,16 @@ import Input from "./components/Input";
 const API_KEY = "5d5c88002278a4fbef29d7752e855e0e";
 
 const getAPI_RequestString = (city) =>
-  `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+  `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
 
 const emptyQueryData = {
-  country: "",
-  city: "",
+  City: "",
 };
 
 export default function App() {
   const [queryData, setQueryData] = useState(emptyQueryData);
   const [responseData, setResponseData] = useState({});
+  const [errorData, setErrorData] = useState("");
 
   const handleChange = (id, e) => {
     const newData = { ...queryData };
@@ -22,36 +22,68 @@ export default function App() {
     setQueryData(newData);
   };
 
-  const handleSearch = async () => {
-    const api = await fetch(getAPI_RequestString(queryData.city));
-    const data = await api.json();
+  const handleError = ({ cod, message }) => {
+    if (cod >= 400 && cod <= 599) {
+      // client or server error response
+      throw new Error(`${cod}, Message: ${message}`);
+    }
+  };
 
-    const newResponseData = { ...responseData, ...data.main };
-    setResponseData(newResponseData);
+  const handleSearch = async () => {
+    try {
+      const api = await fetch(getAPI_RequestString(queryData.City));
+      const data = await api.json();
+
+      // handle any error responses
+      handleError(data);
+      // if no errors thrown, set no errors
+      setErrorData("");
+
+      setResponseData({ ...responseData, ...data.main });
+
+    } catch (e) {
+      console.error(e);
+      setErrorData(e.toString());
+    }
+  };
+
+  const handleEnter = (e) => {
+    if (e.key === "Enter") {
+      handleSearch();
+    }
   };
 
   return (
     <div className="container mt-5">
-      <h1 align="center">Weather App</h1>
-      {Object.keys(queryData).map((key) => (
-        <Input
-          key={key}
-          id={key}
-          label={key}
-          type="text"
-          placeholder="Input text here"
-          value={queryData[key]}
-          onChange={handleChange}
-        />
-      ))}
-      <button className="btn btn-primary align-self-end" onClick={handleSearch}>
-        Search
-      </button>
-      {Object.keys(responseData).map((key) => (
-        <p key={key}>
-          {key}: {responseData[key]}
-        </p>
-      ))}
+      <h1>Weather App</h1>
+      <div className="row">
+        <div className="col">
+          {Object.keys(queryData).map((key) => (
+            <Input
+              key={key}
+              id={key}
+              label={key}
+              type="text"
+              placeholder="Input text here"
+              value={queryData[key]}
+              onChange={handleChange}
+              onKeyDown={handleEnter}
+            />
+          ))}
+          {errorData && (
+            <div className="alert alert-danger" role="alert">
+              {errorData}
+            </div>
+          )}
+          <button className="btn btn-primary" onClick={handleSearch}>
+            Search
+          </button>
+        </div>
+        <div className="col">
+          <p className="temp">{responseData.temp}</p>
+          <span className="temp">&#8451;</span>
+        </div>
+      </div>
     </div>
   );
 }
