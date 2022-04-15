30c0bdfc6a76c022d62164c199c2dc4a1e6823d2
Ryan Tabar <70136507+SirFourier@users.noreply.github.com>
2022-04-15 11:25:20 +0100

    added radio to control input type

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,42 @@
 import React, { useState } from "react";
 import Input from "./components/Input";
+import MultipleInputs from "./components/MultipleInputs";
+import RadioGroup from "./components/RadioGroup";
 
 // Using openweathermap.org
-const API_KEY = "5d5c88002278a4fbef29d7752e855e0e";
-
-const getAPI_RequestString = (city) =>
-  `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
-
-const emptyQueryData = {
-  City: "",
+const API = {
+  KEY: "5d5c88002278a4fbef29d7752e855e0e",
+  UNITS: "metric",
+  ROOT: "https://api.openweathermap.org/data/2.5/weather?",
+  queryByCity: (city) =>
+    `${API.ROOT}q=${city}&appid=${API.KEY}&units=${API.UNITS}`,
+  queryByCoordinates: (lat, lon) =>
+    `${API.ROOT}lat=${lat}&lon=${lon}&appid=${API.KEY}&units=${API.UNITS}`,
 };
 
+const emptyQuery = {
+  city: "",
+  coordinates: { latitude: "", longitude: "" },
+};
+
+//london
+//lat: 51.509865
+//lon: -0.118092
+
 export default function App() {
-  const [queryData, setQueryData] = useState(emptyQueryData);
-  const [responseData, setResponseData] = useState({});
-  const [errorData, setErrorData] = useState("");
+  const [query, setQuery] = useState(emptyQuery);
+  const [queryType, setQueryType] = useState("city");
+  const [weather, setWeather] = useState({});
+  const [error, setError] = useState("");
 
   const handleChange = (id, e) => {
-    const newData = { ...queryData };
-    newData[id] = e.target.value;
-    setQueryData(newData);
+    const newQuery = { ...query };
+    if (id === "city") {
+      newQuery[id] = e.target.value;
+    } else if (id === "latitude" || id === "longitude") {
+      newQuery.coordinates[id] = e.target.value;
+    }
+    setQuery(newQuery);
   };
 
   const handleError = ({ cod, message }) => {
@@ -31,19 +48,26 @@ export default function App() {
 
   const handleSearch = async () => {
     try {
-      const api = await fetch(getAPI_RequestString(queryData.City));
+      const api = await fetch(
+        queryType === "city"
+          ? API.queryByCity(query.city)
+          : API.queryByCoordinates(
+              query.coordinates.latitude,
+              query.coordinates.longitude
+            )
+      );
       const data = await api.json();
 
       // handle any error responses
       handleError(data);
       // if no errors thrown, set no errors
-      setErrorData("");
+      setError("");
+      console.log(data);
 
-      setResponseData({ ...responseData, ...data.main });
-
+      setWeather({ ...weather, ...data });
     } catch (e) {
       console.error(e);
-      setErrorData(e.toString());
+      setError(e.toString());
     }
   };
 
@@ -53,37 +77,56 @@ export default function App() {
     }
   };
 
+  const handleRadioChange = (key) => {
+    setQueryType(key);
+  };
+
   return (
     <div className="container mt-5">
       <h1>Weather App</h1>
-      <div className="row">
-        <div className="col">
-          {Object.keys(queryData).map((key) => (
-            <Input
-              key={key}
-              id={key}
-              label={key}
-              type="text"
-              placeholder="Input text here"
-              value={queryData[key]}
-              onChange={handleChange}
-              onKeyDown={handleEnter}
-            />
-          ))}
-          {errorData && (
-            <div className="alert alert-danger" role="alert">
-              {errorData}
-            </div>
-          )}
-          <button className="btn btn-primary" onClick={handleSearch}>
-            Search
-          </button>
+      <RadioGroup
+        options={query}
+        checked={queryType}
+        onChange={handleRadioChange}
+      />
+
+      {queryType === "city" ? (
+        <Input
+          id={"city"}
+          label={"City"}
+          type="text"
+          placeholder="city, (country is optional)"
+          value={query.city}
+          onChange={handleChange}
+          onKeyDown={handleEnter}
+        />
+      ) : (
+        <MultipleInputs
+          type="text"
+          mainLabel="Coordinates"
+          values={query.coordinates}
+          onChange={handleChange}
+          onKeyDown={handleEnter}
+        />
+      )}
+
+      {error && (
+        <div className="alert alert-danger" role="alert">
+          {error}
         </div>
-        <div className="col">
-          <p className="temp">{responseData.temp}</p>
+      )}
+      <button className="btn btn-primary" onClick={handleSearch}>
+        Search
+      </button>
+      {weather.main && (
+        <div>
+          <p className="temp">{Math.round(weather.main.temp)}</p>
           <span className="temp">&#8451;</span>
+          <p>
+            {weather.name}, {weather.sys.country}
+          </p>
         </div>
-      </div>
+      )}
     </div>
   );
 }
