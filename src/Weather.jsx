be48f43ac62774866286ad3e5a7a307c3c43c7fa
Ryan Tabar <70136507+SirFourier@users.noreply.github.com>
2022-05-22 03:59:52 +0100

    small code layout change

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -7,9 +7,7 @@ export default function Weather() {
   return (
     <div className="container">
       <div className="mt-3 row">
-        <Current
-          className="col-auto"
-        />
+        <Current className="col-auto" />
         <Daily className="col" />
       </div>
     </div>
